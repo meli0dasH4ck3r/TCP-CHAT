c42fdf3a8ed73c4cb4d4ec7a0f6b39b430dd9f8d
Dimdim9 <madi.hamidou13@gmail.com>
2025-01-07 14:30:01 +0100

    Update testing.py

--- testing.py
+++ testing.py
@@ -2,6 +2,6 @@
 
 if __name__ == "__main__":
     chat_server = ChatServer()
-    threading.thred(targer=chat_server.receive_clients).start()
+    threading.Thred(targer=chat_server.receive_clients).start()
 
 #placeholder for clien integration and testing 
