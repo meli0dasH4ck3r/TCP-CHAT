291034ad0dd62859d89bf10873fe29889433e57e
Dimdim9 <madi.hamidou13@gmail.com>
2025-01-07 13:48:47 +0100

    Create testing.py

---
+++ testing.py
@@ -0,0 +1,7 @@
+# Intergration et testing -Madi
+
+if __name__ == "__main__":
+    chat_server = ChatServer()
+  threading.thred(targer=chat_server.receive_clients).stars()
+
+#placeholder for clien integration and testing 
